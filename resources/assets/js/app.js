91e4c5f93a1acc9418df0f628af9b39154c8b5ec
Alex Porter <alex.porter@teamrock.com>
2015-11-07 21:33:52 +0000

    basic view functionality

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,51 @@
 var Vue = require('vue');
+Vue.use(require('vue-resource'));
 
 new Vue({
     el: "#application",
-    data: {
-        message: 'this is cool!'
-    }
 
+    data: {
+        todos: {},
+        newTodo: {
+            todoName: '',
+            todoDescription: ''
+        },
+        submitted: false
+    },
+
+    computed: {
+        errors: function() {
+            for(var key in this.newTodo) {
+                if(! this.newTodo[key]) return true;
+            }
+        }
+    },
+
+    ready: function () {
+        this.getTodos()
+    },
+    
+    methods: {
+        getTodos: function () {
+            this.$http.get('http://localhost:10000/', function (todos) {
+                this.todos = todos;
+            });
+        },
+
+        onFormSubmit: function(e){
+            e.preventDefault();
+            var todo = this.newTodo;
+
+            this.$http.post('http://localhost:10000/add', todo);
+
+            this.submitted = true;
+
+            this.newTodo = {
+                'todoName' : "",
+                'todoDescription' : ""
+            };
+
+        }
+    }
 
 });
