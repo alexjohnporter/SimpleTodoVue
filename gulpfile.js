fe655864664a7986ac3c69c683bed8aac2cd5c04
Alex Porter <alex.porter@teamrock.com>
2015-11-07 18:55:38 +0000

    basic browserfy set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var elixir = require('laravel-elixir');
 
-elixir(function(mix){
+elixir(function(mix) {
+    mix.scripts([
+        '../../../vendor/bootstrap/dist/js/bootstrap.min.js',
+        '../../../vendor/jquery/dist/jquery.min.js'
+    ], 'public/js/vendor.js');
 
+    mix.styles([
+        '../../../vendor/bootstrap/dist/css/bootstrap.min.css'
+    ], 'public/css/vendor.css');
+
+    mix.browserify('app.js');
 });
---
+++ resources/assets/js/app.js
@@ -0,0 +1,10 @@
+var Vue = require('vue');
+
+new Vue({
+    el: "#application",
+    data: {
+        message: 'this is cool!'
+    }
+
+
+});
